36921718fd5eef7ae27fa7c5c0a5fef7c8831806
Kirill <fka@mail.ru>
2025-04-01 20:31:23 +0300

    mission15_2_1

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,8 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- set default_schema = target.schema -%}
+
     {%- if custom_schema_name is none -%}
         {{ default_schema }}
     {%- else -%}
-        {{ custom_schema_name | trim }}
+        {%- if target.name == 'prod' -%}
+            {{ custom_schema_name | trim }}
+        {%- else -%}
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+        {%- endif -%}
     {%- endif -%}
 {%- endmacro %}
